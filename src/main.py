9e48961293ebc41830fbd2ee8fb053213b1764f3
jsowers13 <jsowers13@gmail.com>
2022-05-10 02:40:39 +0000

    redid project

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Starships
 #from models import Person
 
 app = Flask(__name__)
@@ -39,6 +39,53 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+
+    characters_query = Characters.query.all()
+
+    all_characters = list(map(lambda character: character.serialize(), characters_query))
+
+    return jsonify(all_characters), 200
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+    character = Characters.query.get(character_id)
+
+    return jsonify(character.serialize()), 200
+
+    # user1 = Person.query.get(person_id)
+    #     return jsonify(user1.serialize()), 200
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+
+    planets_query = Planets.query.all()
+
+    all_planets = list(map(lambda planet: planet.serialize(), planets_query))
+
+    return jsonify(all_planets), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+
+    return jsonify(planet.serialize()), 200
+
+@app.route('/starships', methods=['GET'])
+def get_all_starships():
+
+    starships_query = Starships.query.all()
+
+    all_starships = list(map(lambda starship: starship.serialize(), starships_query))
+
+    return jsonify(all_starships), 200
+
+@app.route('/starships/<int:starship_id>', methods=['GET'])
+def get_starship(starship_id):
+    starship = Starships.query.get(starship_id)
+
+    return jsonify(starship.serialize()), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -17,3 +18,102 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    __tablename__='Characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.String(120), unique=False, nullable=False)
+    mass = db.Column(db.String(120), unique=False, nullable=False)
+    hair_color = db.Column(db.String(120), unique=False, nullable=False)
+    skin_color = db.Column(db.String(120), unique=False, nullable=False)
+    eye_color = db.Column(db.String(120), unique=False, nullable=False)
+    gender = db.Column(db.String(120), unique=False, nullable=False)
+    birth_year = db.Column(db.String(120), unique=False, nullable=False)
+    homeworld = db.Column(db.String(120), unique=False, nullable=True)
+    image_url = db.Column(db.String(250), unique=True, nullable=True)
+
+    def serialize(self):
+            return {
+                "id": self.id,
+                "name": self.name,
+                "height": self.height,
+                "mass": self.mass,
+                "hair_color": self.hair_color,
+                "skin_color": self.skin_color,
+                "eye_color": self.eye_color,
+                "gender": self.gender,
+                "birth_year": self.birth_year,
+                "homeworld": self.homeworld,
+                "image_url": self.image_url
+
+            }
+
+class Planets(db.Model):
+    __tablename__='Planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    rotation_period = db.Column(db.String(120), unique=False, nullable=False)
+    orbital_period = db.Column(db.String(120), unique=False, nullable=False)
+    diameter = db.Column(db.String(120), unique=False, nullable=False)
+    climate = db.Column(db.String(120), unique=False, nullable=False)
+    gravity = db.Column(db.String(120), unique=False, nullable=False)
+    terrain = db.Column(db.String(120), unique=False, nullable=False)
+    surface_water = db.Column(db.String(120), unique=False, nullable=False)
+    population = db.Column(db.String(120), unique=False, nullable=False)
+    image_url = db.Column(db.String(250), unique=True, nullable=True)
+
+    def serialize(self):
+            return{
+                "id": self.id,
+                "name": self.name,
+                "rotation_period": self.rotation.period,
+                "orbital_period": self.orbital_period,
+                "diameter": self.diameter,
+                "climate": self.climate,
+                "gravity": self.gravity,
+                "terrain": self.terrain,
+                "surface_water": self.surface_water,
+                "population": self.population,
+                "image_url": self.image_url
+
+            }
+
+class Starships(db.Model):
+    __tablename__='Starships'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    model = db.Column(db.String(120), unique=True, nullable=False)
+    manufacturer = db.Column(db.String(120), unique=False, nullable=False)
+    cost_in_credits = db.Column(db.String(120), unique=False, nullable=False)
+    length = db.Column(db.String(120), unique=False, nullable=False)
+    max_atmosphering_speed = db.Column(db.String(120), unique=False, nullable=False)
+    crew = db.Column(db.String(120), unique=False, nullable=False)
+    passengers = db.Column(db.String(120), unique=False, nullable=False)
+    cargo_capacity = db.Column(db.String(120), unique=False, nullable=False)
+    consumables = db.Column(db.String(120), unique=False, nullable=False)
+    hyperdrive_rating = db.Column(db.String(120), unique=False, nullable=False)
+    MGLT = db.Column(db.String(120), unique=False, nullable=False)
+    starship_class = db.Column(db.String(120), unique=False, nullable=False)
+    pilots = db.Column(db.String(120), unique=False, nullable=True)
+    image_url = db.Column(db.String(250), unique=True, nullable=True)
+
+    def serialize(self):
+            return{
+                "id": self.id,
+                "name": self.name,
+                "model": self.model,
+                "manufacturer": self.manufacturer,
+                "cost_in_credits": self.cost_in_credits,
+                "length": self.length,
+                "max_atmosphering_speed": self.max_atmosphering_speed,
+                "crew": self.crew,
+                "passengers": self.passengers,
+                "cargo_capacity": self.cargo_capacity,
+                "consumables": self.consumables,
+                "hyperdrive_rating": self.hyperdrive_rating,
+                "MGLT": self.MGLT,
+                "starship_class": self.starship_class,
+                "pilots": self.pilots,
+                "image_url": self.image_url
+            }
